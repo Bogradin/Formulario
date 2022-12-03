4c0e86d81f6e881102b525673ebcf05c06f4ec18
Lucas Inacio <lucasinacio2003@hotmail.com>
2022-12-03 05:44:42 -0300

    Select City

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ window.onload = populateSelect()
 async function populateSelect() {
     let response = await fetch('https://amazon-api.sellead.com/country')
     let users = await response.json()
-    console.log(users[1].code)
 
     let element = document.getElementById('selectCountry');
     for (let index = 0; index < users.length; index++) {
@@ -13,3 +12,20 @@ async function populateSelect() {
     }
 }
 
+async function populateSelectCities() {
+    selectElement = document.querySelector('#selectCountry');
+    output = selectElement.value;
+
+    let response = await fetch('https://amazon-api.sellead.com/city?country_code=' + output + '&forAccommodation=true')
+    let users = await response.json()
+    console.log(users)
+
+    let element = document.getElementById('selectCity');
+    for (let index = 0; index < users.length; index++) {
+        // BIND DATA TO <select> ELEMENT.
+        element.innerHTML = element.innerHTML +
+            '<option value="' + users[index].code + '">' + users[index].name + '</option>';
+    }
+}
+
+populateSelectCities()
