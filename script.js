f7e6767d1dde5266c0fd510c4454217066b54bd9
Lucas Inacio <lucasinacio2003@hotmail.com>
2022-12-03 21:28:24 -0300

    Better layout

--- script.js
+++ script.js
@@ -15,17 +15,19 @@ async function populateSelect() {
 async function populateSelectCities() {
     selectElement = document.querySelector('#selectCountry');
     output = selectElement.value;
-
-    var i, L = selectCity.options.length - 1;
-    for(i = L; i >= 0; i--) {
-        selectCity.remove(i);
-    }
+    let element = document.getElementById('selectCity');
 
     let response = await fetch('https://amazon-api.sellead.com/city?country_code=' + output + '&forAccommodation=true')
     let users = await response.json()
     console.log(users)
 
-    let element = document.getElementById('selectCity');
+    var i, L = selectCity.options.length - 1;
+    for(i = L; i >= 0; i--) {
+        selectCity.remove(i);
+    }
+    element.innerHTML = element.innerHTML +
+            '<option value="' + 1 + '">' + 'City' + '</option>';
+    
     for (let index = 0; index < users.length; index++) {
         // BIND DATA TO <select> ELEMENT.
         element.innerHTML = element.innerHTML +
